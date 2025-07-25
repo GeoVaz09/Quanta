9c489bc4df8b3f3d217d0c327cfc5edfe99baa63
George Vazakas <vazakasgeorge@gmail.com>
2025-07-25 23:27:03 +0300

    v.0.4.9
    many syntax changes, added float boolean string variables ans inputs, new commands, almost ready for my first game "I believe :)"

--- Quanta/Quanta.py
+++ Quanta/Quanta.py
@@ -1,8 +1,13 @@
+import random
+import time
+
 cursor = 0
 variables = []
 run = True
 
-with open('program.txt') as f:
+file = input("File to load: ")
+
+with open(file) as f:
     program = [line.rstrip() for line in f]
 
 while run:
@@ -22,6 +27,17 @@ while run:
 #        print("Comment:", command[1])
         cursor = cursor + 1
 
+    elif command[0] == "random":
+        variables[int(command[3])] = random.randint(int(command[1]), int(command[2]))
+
+    elif command[0] == "wait":
+        if command[1] == "$":
+            time.sleep(variables[int(command[2])])
+            cursor = cursor + 1
+        else:
+            time.sleep(command[1])
+            cursor = cursor + 1
+
     elif command[0] == "goto":
         if int(command[1]) == cursor:
             print("Error: Infinite goto loop on itself")
@@ -31,17 +47,35 @@ while run:
             cursor = int(command[1])
 
     elif command[0] == "var":
-        for i in range(int(command[1])):
-            variables.append(None)
+        if command[1] == "$":
+            for i in range(variables[int(command[1])]):
+                variables.append(None)
+        else:
+            for i in range(int(command[1])):
+                variables.append(None)
         cursor = cursor + 1
 
     elif command[0] == "set":
-        variables[int(command[1])] = command[2]
+        if command[1] == "int":
+            variables[int(command[2])] = int(command[3])
+        elif command[1] == "str":
+            variables[int(command[2])] = str(command[3])
+        elif command[1] == "float":
+            variables[int(command[2])] = float(command[3])
+        elif command[1] == "bool":
+            variables[int(command[2])] = bool(command[3])
         cursor = cursor + 1
 
     elif command[0] == "input":
-        print(command[2])
-        variables[int(command[1])] = input("Type something: ")
+        print(command[3])
+        if command[1] == "int":
+            variables[int(command[2])] = int(input("Type something: "))
+        elif command[1] == "str":
+            variables[int(command[2])] = str(input("Type something: "))
+        elif command[1] == "float":
+            variables[int(command[2])] = float(input("Type something: "))
+        elif command[1] == "bool":
+            variables[int(command[2])] = bool(input("Type something: "))
         cursor = cursor + 1
 
     elif command[0] == "end;":
@@ -50,17 +84,84 @@ while run:
 
     elif command[0] == "math":
         if command[1] == "+":
-            variables[int(command[4])] = int(variables[int(command[2])]) + int(variables[int(command[3])])
+            variables[int(command[4])] = variables[int(command[2])] + variables[int(command[3])]
             cursor = cursor + 1
         elif command[1] == "-":
-            variables[int(command[4])] = int(variables[int(command[2])]) - int(variables[int(command[3])])
+            variables[int(command[4])] = variables[int(command[2])] - variables[int(command[3])]
             cursor = cursor + 1
         elif command[1] == "*":
-            variables[int(command[4])] = int(variables[int(command[2])]) * int(variables[int(command[3])])
+            variables[int(command[4])] = variables[int(command[2])] * variables[int(command[3])]
             cursor = cursor + 1
         elif command[1] == "/":
-            variables[int(command[4])] = int(variables[int(command[2])]) / int(variables[int(command[3])])
+            variables[int(command[4])] = variables[int(command[2])] / variables[int(command[3])]
             cursor = cursor + 1
+
+    elif command[0] == "check":
+        if command[1] == "==":
+            if variables[int(command[2])] == variables[int(command[3])]:
+                if int(command[4]) == cursor:
+                    print("Error: Infinite goto loop on itself")
+                    run = False
+        
+                else:
+                    cursor = int(command[4])
+            else:
+                cursor = cursor + 1
+
+        elif command[1] == "!=":
+            if variables[int(command[2])] == variables[int(command[3])]:
+                if int(command[4]) == cursor:
+                    print("Error: Infinite goto loop on itself")
+                    run = False
+        
+                else:
+                    cursor = int(command[4])
+            else:
+                cursor = cursor + 1
+
+        elif command[1] == "<":
+            if variables[int(command[2])] == variables[int(command[3])]:
+                if int(command[4]) == cursor:
+                    print("Error: Infinite goto loop on itself")
+                    run = False
+        
+                else:
+                    cursor = int(command[4])
+            else:
+                cursor = cursor + 1
+
+        elif command[1] == ">":
+            if variables[int(command[2])] == variables[int(command[3])]:
+                if int(command[4]) == cursor:
+                    print("Error: Infinite goto loop on itself")
+                    run = False
+        
+                else:
+                    cursor = int(command[4])
+            else:
+                cursor = cursor + 1
+
+        elif command[1] == "<=":
+            if variables[int(command[2])] == variables[int(command[3])]:
+                if int(command[4]) == cursor:
+                    print("Error: Infinite goto loop on itself")
+                    run = False
+        
+                else:
+                    cursor = int(command[4])
+            else:
+                cursor = cursor + 1
+
+        elif command[1] == ">=":
+            if variables[int(command[2])] == variables[int(command[3])]:
+                if int(command[4]) == cursor:
+                    print("Error: Infinite goto loop on itself")
+                    run = False
+        
+                else:
+                    cursor = int(command[4])
+            else:
+                cursor = cursor + 1
 
     elif command[0] == "break;":
         user = input('Press Enter to continue or type "q" to exit: ')
